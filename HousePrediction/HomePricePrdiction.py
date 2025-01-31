c12b7c593ba087186bda9c4786ff2a666ea8917c
Mueez <mueezzakir6@gmail.com>
2025-01-31 16:45:59 +0500

    update

---
+++ HousePrediction/HomePricePrdiction.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('C:/Users/mueez/OneDrive/Desktop/Machine/HousePrediction/file.csv');
+print(df)
+
+#Distribution of Data
+plt.xlabel('Area')
+plt.ylabel('Price')
+plt.scatter(df.Area, df.Price, color='red', marker='x')
+plt.show()
+
+reg = LinearRegression()
+reg.fit(df[['Area']], df['Price'])  #df[['Area']] → Selects the Area column as a DataFrame (needed for Scikit-Learn). df['Price'] → Selects the Price column as the target (y).
+y = reg.predict(pd.DataFrame([[3000]], columns=['Area']))
+print(y)
--- HousePredition/HomePricePrdiction.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
-df = pd.read_csv('/HousePredition/file.csv');
-print(df)
