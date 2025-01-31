fb2af74bc8c59ea73ce75638ed1412aa5db90804
Mueez <mueezzakir6@gmail.com>
2025-01-31 17:22:37 +0500

    update

--- HousePrediction/HomePricePrdiction.py
+++ HousePrediction/HomePricePrdiction.py
@@ -10,9 +10,15 @@ print(df)
 plt.xlabel('Area')
 plt.ylabel('Price')
 plt.scatter(df.Area, df.Price, color='red', marker='x')
-plt.show()
+# plt.show()
 
 reg = LinearRegression()
 reg.fit(df[['Area']], df['Price'])  #df[['Area']] → Selects the Area column as a DataFrame (needed for Scikit-Learn). df['Price'] → Selects the Price column as the target (y).
 y = reg.predict(pd.DataFrame([[3000]], columns=['Area']))
 print(y)
+
+print(reg.coef_) # shows the value of w aka m ie slope
+print(reg.intercept_) # shows the value of b 
+
+# mx + b 
+print(135.78767123 * 3000 + 180616.43835616432)
